1853b8a39a37bd7230270ae19f277edd76a3679c
mtmjr92 <>
2021-05-12 11:46:18 -0300

    Spring Boot 2 Essentials 13 - Spring Data JPA pt 01

--- src/main/java/academy/devdojo/springboot2/domain/Anime.java
+++ src/main/java/academy/devdojo/springboot2/domain/Anime.java
@@ -2,13 +2,23 @@ package academy.devdojo.springboot2.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 // gets, sets, equals, toString
 @Data
 // construtor com todos os valores
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Anime {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
